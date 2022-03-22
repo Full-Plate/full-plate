3c8723268f49f18cb6789692080625069273b925
emcdanie <66241957+emcdanie@users.noreply.github.com>
2022-03-22 16:21:22 +0100

    Navbar
    Created Navbar, with logo, and clickable links

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 
-import './App.css';
+import React from "react";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      hello world
+      <Router>
+        <Navbar />
+        
+      </Router>
     </div>
   );
 }
