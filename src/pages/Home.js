a0da2635693e76261065ff7481e1067ae537aa78
Sean Sipus <sean.sipus@gmail.com>
2022-03-23 15:24:55 -0400

    Add container for text on main page in Home.js, add styling to Home.css container

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,16 +8,16 @@ function Home() {
   return (
     <div className="home" style={{ backgroundImage: `url(${BannerImage})` }}>
       <div className="headerContainer">
-        <Wrapper>
+        <div className="homePageTextContainer">
           <h1 style={{ fontWeight: "bold" }}> Making the Most of what you Have </h1>
           <p>We are here to help you find ways to use all the ingredients you 
             have on hand. We believe that mindful meal planning is not confined
             to your kitchen. Join us on a journey towards a Full Plate - both for yourself
             and for others</p>
+          </div>
           <Link to="/Recipes">
             <button> Get Started</button>
           </Link>
-        </Wrapper>
       </div>
     </div>
   );
