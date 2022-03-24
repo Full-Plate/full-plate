a5f0f9080160f9135a8c77cfccf556d2dac02911
emcdanie <66241957+emcdanie@users.noreply.github.com>
2022-03-24 21:22:53 +0100

    recipe tiles

--- src/components/RecipeTile.js
+++ src/components/RecipeTile.js
@@ -8,17 +8,18 @@ function RecipeTile({ recipe }) {
     
     recipe["recipe"]["image"].match(/\.(jpeg|jpg|gif|png)$/) != null && (
       
-      <div className="recipeTile">
-       
+      <article className="recipeTile">
+       < div className = 'img-container' >
         <img
           className="recipeTile__image"
           src={recipe["recipe"]["image"]}
           alt="tile-image"
           onClick={() => window.open(recipe["recipe"]["url"])}
-        />
+        /> </div >
+        <div className='recipe-footer'>
         <p className="recipeTile__name">{recipe["recipe"]["label"]}</p>
-        
-      </div>
+        </div>
+      </article>
       
     )
     
