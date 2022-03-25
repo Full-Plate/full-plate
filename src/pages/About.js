4d36947ca5ec49689b8731dede8dcb01206cda54
emcdanie <66241957+emcdanie@users.noreply.github.com>
2022-03-25 17:04:02 +0100

    About page
    Added the text and images.
    Working on the grid.

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,23 +1,44 @@
 import React from "react";
-import MultiplePizzas from "../assets/masala.jpg";
+
+//images
+import maps from "../assets/maps.png";
+import search from "../assets/search.png";
+
+
 import "../styles/About.css";
+
+
 function About() {
   return (
-    <div className="about">
-      <div
-        className="aboutTop"
-        style={{ backgroundImage: `url(${MultiplePizzas})` }}
-      ></div>
-      <div className="aboutBottom">
-        <h1> ABOUT US</h1>
-        <p>
-          At Full Plate, we curate meal ideas based on what you already have on hand, so you can spend your time and energy savoring rather than stressing.Since delicious food is meant to be shared, we’ ve added a bonus route
-          for unused ingredients by showing where you can donate to help fight food insecurity in the community.
-
-        </p>
-      </div>
+  <div class="container-about">
+    <h1>
+       Here is how we can help!
+      </h1>
+  <div class="card-about">
+    <div class="card-header-about-image">
+       <img class="search" src={search} alt="search-image" />
     </div>
+    <div class="card-body-about">
+      <p>
+        At Full Plate, we curate meal ideas based on what you already have on hand, so you can spend your time and energy savoring rather than stressing.
+      </p>
+      </div>
+      
+      <div class="card-about-image">
+         <img class="maps" src={maps} alt="maps-image" />
+       < div class = "card-body-about" >
+          <p>
+        Sharing is caring. Here is an alternative for your unused ingredients so you can donate to hep your community.
+      </p>
+        </div>
+     
+    </div>
+  </div>
+   <h1>
+       Meet the Team!
+      </h1>
+  </div>
   );
-}
+  }
 
 export default About;
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,17 +1,16 @@
 import React from "react";
-import PizzaLeft from "../assets/fooda.jpeg";
+
 import "../styles/Contact.css";
+
+//Images
+
 
 function Contact() {
   return (
     <div className="contact">
-      <div
-        className="leftSide"
-        style={{ backgroundImage: `url(${PizzaLeft})` }}
-      ></div>
+    
       <div className="rightSide">
         <h1> Contact Us</h1>
-
         <form id="contact-form" method="POST">
           <label htmlFor="name">Full Name</label>
           <input name="name" placeholder="Enter full name..." type="text" />
