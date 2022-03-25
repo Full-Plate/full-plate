effa1f0d5d41fc01665f188d88e55d016671da3e
emcdanie <66241957+emcdanie@users.noreply.github.com>
2022-03-25 11:32:10 +0100

    updated logo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import Logo from "../assets/logo.svg";
+import Logo from "../assets/logo.png";
 import { Link } from "react-router-dom";
 import MenuIcon from '@mui/icons-material/Menu';
 import "../styles/Navbar.css";
@@ -13,11 +13,11 @@ function Navbar() {
   return (
     <div className="navbar">
       <div className="leftSide" id={openLinks ? "open" : "close"}>
-        <img src={Logo} />
+        <img src={Logo}/>
         <div className="hiddenLinks">
           <ul>
             <li>
-              <Link to="/"> Home </Link>
+              <Link to = "/" > Home </Link>
               <Link to="/about"> About </Link>
               <Link to="/Recipes"> Recipes </Link>
               <Link to = "/Donate" > Donate Food </Link>
