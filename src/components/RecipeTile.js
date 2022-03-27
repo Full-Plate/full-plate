b3d1e710efddca1cfc931159c629c8f1973b34cb
emcdanie <66241957+emcdanie@users.noreply.github.com>
2022-03-27 18:54:59 +0200

    css issue resolved

--- src/components/RecipeTile.js
+++ src/components/RecipeTile.js
@@ -7,16 +7,16 @@ function RecipeTile({ recipe }) {
   return (
     
     recipe["recipe"]["image"].match(/\.(jpeg|jpg|gif|png)$/) != null && (
-      <div className="card-container1">
-      <div className="card">
+      <div className="tile-card-container1">
+      <div className="tile-card">
 			<img
-          className="recipeTile-image"
+          className="tile-recipeTile-image"
           src={recipe["recipe"]["image"]}
           alt="tile-image"
           onClick={() => window.open(recipe["recipe"]["url"])}
         />
-        <div className="card-body">
-			<h2 className="card-title ">
+        <div className="tile-card-body">
+			<h2 className="tile-card-title ">
 				<p>{recipe["recipe"]["label"]}</p>
 			</h2>
 		</div>
