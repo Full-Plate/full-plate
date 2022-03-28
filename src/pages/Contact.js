eff3151bacdb1de8e02f1854c6667a7462e06923
emcdanie <66241957+emcdanie@users.noreply.github.com>
2022-03-28 11:17:55 +0200

    Contact css
    Organized css on contact page

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -6,43 +6,44 @@ import "../styles/Contact.css";
 
 function Contact() {
   return (
+    <>
     <div className="contact-container">
        <h1 className="contact-header">We would love to hear from you!</h1>
-        <div className="contact-text">
+       </div>
+       <div>
          <p className= "contact-text"> Got Something you can not wait to share with us </p>
           <p className = "contact-text" > Be it Suggestions, feedback, business opportunities or
              even how our recipes made you hungry, we would love to hear from you!</p>
-             </div> 
-     <div class="card">
+             </div>
+     <div className="card">
   <h2>Contact Us</h2>
-  <div class="row">
-    <div class="col">
-      <div class="form-group">
+  <div className="row">
+    <div className="col">
+      <div className="form-group">
         < label > How do we address you ? </label>
         <input type="text"/>
       </div>
     </div>
 
-    <div class="col">
-      <div class="form-group">
+    <div className="col">
+      <div className="form-group">
         <label>Your E-mail</label>
         <input type="text"/>
       </div>
     </div>
-    <div class="col">
-      <div class="form-group">
+    <div className="col">
+      <div className="form-group">
         <label>What would you like to say?</label>
         <textarea></textarea>
       </div>
     </div>
 
-    <div class="col">
+    <div className="col">
       <input type="submit" value="Send" />
     </div>
   </div>
-</div>
-    </div>  
-    
+</div> 
+    </>
   );
 }
 
