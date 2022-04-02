e245e35fa1ceef2e8bdd532c84f3d7e2d85ba526
emcdanie <66241957+emcdanie@users.noreply.github.com>
2022-04-02 07:36:31 +0200

    home
    added the Banner in the background

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import BannerImage from "../assets/Background.svg";
+import BannerImage from "../assets/banner.png";
 import "../styles/Home.css"; 
 
 import homefood  from "../assets/homefood.png";
