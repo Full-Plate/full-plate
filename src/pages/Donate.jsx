10f302e92c3de837cb14acccf7b54c5ed93513b5
emcdanie <66241957+emcdanie@users.noreply.github.com>
2022-04-06 14:18:43 +0200

    Donate page adjustments

--- src/pages/Donate.jsx
+++ src/pages/Donate.jsx
@@ -1,7 +1,8 @@
 import "../styles/Donate.css";
 
-import orginfo from '../assets/orginfo.png';
-import citymap from '../assets/citymap.png';
+import fridge from '../assets/fridge.png';
+import counter from "../assets/counter.png";
+import orginfo from "../assets/orginfo.png";
 
 export default function Donate() {
   return (
@@ -13,19 +14,21 @@ export default function Donate() {
           each year. <strong>103</strong> pounds is about a <stong>MONTH</stong> of meals for another person </p>
           <p className = "donate-text" > Be it Suggestions, feedback, business opportunities or
              even how our recipes made you hungry, we would love to hear from you!</p>
-             </div> 
-             <div className="orginfo">
-             < div className = "orginfoimg">
-              <img src={orginfo} alt="cityfridgephoto" />
+              < div className = "cityfridgeimage">
+              <img src={orginfo} alt="cityfridgeorgimage" />
+             </div>
               </div>
-              <h2 className="donatepageh2">Find your nearest fridge below?</h2>
-              
-              <img className="citymaping" src={citymap} alt="cityfridgemap" />
+            <div className="wrapper">
+           <h1 className="otginfo">Find your nearest fridge today!</h1>
+             < div className = "orginfo-container">
+              <img src={counter} alt="cityfridgephoto" />
+              </div>
+              <h2 className="donatepageh2">Find your nearest fridge today!</h2>
+              <p className = "donate-text-2" > Current NYC Fridge Count</p>
+              <img className="fridge" src={fridge} alt="cityfridgeimage" />
               <button className="mapbutton" onClick={() => window.open("https://nycfridge.com/")}> Take me there now</button>
               </div>
-           
-      </div>
-      </div>
+           </div>
+    </div>
   )
 }
-
