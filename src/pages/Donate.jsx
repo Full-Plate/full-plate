6177cf040d8546f7b5994ad19571d033718701ee
emcdanie <66241957+emcdanie@users.noreply.github.com>
2022-04-06 15:36:47 +0200

    donate page css update

--- src/pages/Donate.jsx
+++ src/pages/Donate.jsx
@@ -9,26 +9,29 @@ export default function Donate() {
     <div className="wrapper">
     <div className="donateContainer">
       <h1 className="donate-header">Why Donate?</h1>
-       <div className="donate-text">
+       <div className="donateinfo-text">
          <p> On average, we throw away <strong>103</strong> pounds of food
           each year. <strong>103</strong> pounds is about a <stong>MONTH</stong> of meals for another person </p>
           <p className = "donate-text" > Be it Suggestions, feedback, business opportunities or
              even how our recipes made you hungry, we would love to hear from you!</p>
-              < div className = "cityfridgeimage">
+              < div className = "nyccomfridgeimage">
               <img src={orginfo} alt="cityfridgeorgimage" />
              </div>
               </div>
             <div className="wrapper">
-              <h1 className="oginfo">Find your nearest fridge today!</h1>
-               <div className = "orginfo-container">
-                 <p className = "donate-text-2" > Current NYC Fridge Count</p>
-                   <div className ="orginfo-image">
+              <h1 className="nearestfridge-header">Find your nearest fridge today!</h1>
+                 <p className = "fridgecount-text" > Current NYC Fridge Count</p>
+                 <div className = "orglink-container">
+                   <div className ="counter-image">
                     <img src={counter} alt="cityfridgephoto" />
                       <button className="donatebutton" onClick={() => window.open("https://nycfridge.com/")}> Take me there now</button>
-                   </div>
+                  
                 </div>
-               <div className ="orginfo-image">
+                <div className = "orglink-container">
+               <div className ="fridge-image">
                     <img className="fridge" src={fridge} alt="cityfridgeimage" />
+                    </div>
+              </div>
               </div>
             </div>
           </div>
