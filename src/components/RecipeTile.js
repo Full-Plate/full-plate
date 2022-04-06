054bb488a5a5ab101799f50734e74d974e0066ab
emcdanie <66241957+emcdanie@users.noreply.github.com>
2022-04-06 11:02:20 +0200

    pulled changes and added from master

--- src/components/RecipeTile.js
+++ src/components/RecipeTile.js
@@ -1,26 +1,33 @@
-
 import React from "react";
 
 import "../styles/RecipeTile.css";
 
 
-  function RecipeTile ({ recipe }) {
+function RecipeTile({
+  recipe
+}) {
   return (
-    recipe.recipe.image && (
-      <div className="tile-card-container1">
-        <div className="tile-card">
-          <img
-            className="tile-recipeTile-image"
-            src={recipe.recipe.image}
-            alt={recipe.recipe.label}
-            onClick={() => window.open(recipe.recipe.url)}
-          />
-          <div className="tile-card-body">
-            <h2 className="tile-card-title ">
-              <p>{recipe.recipe.label}</p>
-            </h2>
-          </div>
-        </div>
+    recipe.recipe.image && ( 
+      <div className = "tile-card-container1" >
+      <div className = "tile-card" >
+      <img className = "tile-recipeTile-image"
+      src = {
+        recipe.recipe.image
+      }
+      alt = {
+        recipe.recipe.label
+      }
+      onClick = {
+        () => window.open(recipe.recipe.url)
+      }
+/> <div className = "tile-card-body" >
+      <h2 className = "tile-card-title " >
+      <p> {
+        recipe.recipe.label
+      } </p> 
+      </h2> 
+      </div> 
+      </div> 
       </div>
     )
   );
