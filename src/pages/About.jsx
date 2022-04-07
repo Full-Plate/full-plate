28de93b9c202cdee7d5fd8d2e3621e8fd209540c
Sean Sipus <sean.sipus@gmail.com>
2022-04-06 22:44:25 -0400

    change styling on about page to match closer to design, add social icons and links to appropriate web pages

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -8,7 +8,9 @@ import search from "../assets/search.png";
 import kristtelle from "../assets/kristtelle.png";
 import delight from "../assets/delight.png";
 import elleta from "../assets/elleta.png";
-
+import sean from "../assets/sean.png";
+import linkedin from "../assets/icons/linkedin.png";
+import mail from "../assets/icons/mail.png"
 
 import "../styles/About.css";
 
@@ -18,7 +20,7 @@ function About() {
   return (
   <>
   <div className="wrapper">
-    <h1 className="about-title">Here is how we can help!</h1>
+    <h1 className="about-title">Here's how we can help</h1>
       <div className="products-container">
         <div className="about-image">
           <img src={search} alt="search.png" />
@@ -36,28 +38,46 @@ function About() {
         </div>
   </div>
   <div className="wrapper">
-   < h2 className = "team-card" >Meet the Team!</h2>
+   < h2 className = "team-card" >Meet the Team</h2>
       
       <div className="team-card-container">
         <div className="team-image-card">
          
           <div className="leftTeamContainer">
-          <img src={kristtelle} alt="kristtelle" />
-          <faCoffee />
+          <img className="headshot" src={kristtelle} alt="kristtelle" />
            <h3 >Kristelle</h3> 
-          <h3> Product Manager</h3>
+          <h3 className="role"> Product Manager</h3>
+          <ul className="social">
+          <li>
+          <a href="mailto:kgumaru2@gmail.com" target="_blank"><img className="icon" src={mail} alt="" />
+              <span>E-mail</span></a> 
+            </li>
+            <li>
+            <a href="https://www.linkedin.com/in/kristelle-gumaru/" target="_blank"><img className="icon" src={linkedin}/>
+             <span>Linkedin</span></a>
+            </li>
+          </ul>
           </div>
           
           <div className="rightTeamContainer">
           <p>Hi! I am kristtelle. At my current job, I work with customers inside and outside of the company to help them with their problems. I have enjoyed it so much that I decided to fully plunge into creating products that truly benefit customers. Food is a big part of my life, so we are excited to share Full Plate with everyone!</p>
-          
           </div>
         </div>
          <div className="team-image-card">
            <div className="leftTeamContainer">
-          <img src={delight} alt="delight"/>
-           <h3>Delight</h3> 
-          <h3>Product Designer</h3>
+          <img className="headshot" src={delight} alt="delight"/>
+           <h3 >Delight</h3> 
+          <h3 className="role">Product Designer</h3>
+          <ul className="social">
+          <li>
+          <a href="mailto:ngdelight.ux@gmail.com" target="_blank"> <img className="icon" src={mail} alt="" />
+              <span>E-mail</span></a> 
+            </li>
+            <li>
+            <a href="https://www.linkedin.com/in/ngdelight/" target="_blank"><img className="icon" src={linkedin}/>
+             <span>Linkedin</span></a>
+            </li>
+          </ul>
           </div>
             <div className="rightTeamContainer">
             <p>Hi! I am Delight and I am delighted to meet you! Coming from a service background, there are 2 things in this world that excite me. Figuring out solutions to problems through experience design and food. What a better project to combine them, than Full Plate. 
@@ -66,24 +86,39 @@ function About() {
         </div>
          <div className="team-image-card">
            <div className="leftTeamContainer">
-          <img src={elleta} alt="elleta.png" />
+          <img className="headshot" src={elleta} alt="elleta.png" />
           <h3>Elleta</h3> 
-          <h3>Software Developer</h3>
+          <h3 className="role">Software Developer</h3>
+          <ul className="social">
+          <li>
+          <a href="mailto:elletamc@gmail.com" target="_blank"><img className="icon" src={mail} alt="" />
+              <span>E-mail</span></a>
+            </li>
+            <li>
+            <a href="https://www.linkedin.com/in/elleta-mcdaniel-7527172a/" target="_blank"><img className="icon" src={linkedin}/>
+             <span>Linkedin</span></a> 
+            </li>
+          </ul>
           </div>
             <div className="rightTeamContainer">
-            < p > Hi! I am Elleta. I am a Front - end Software Developer with 1 - year of experience who is adept in bringing forth expertise in the design, installation, testing, and maintenance of software systems.I am proficient in various platforms, languages, and embedded systems.Experienced with the latest cutting - edge development tools and procedures.Able to effectively self - manage during independent projects, as well as collaborate as part of a productive team.I am an active and curious individual with a passion for learning and self-improvement.
+            < p > Hi! I am Elleta. I am a Front - end Software Developer with 1 - year of experience who is adept in bringing forth expertise in the design, installation, testing, and maintenance of software systems. I am proficient in various platforms, languages, and embedded systems.Experienced with the latest cutting - edge development tools and procedures. Able to effectively self - manage during independent projects, as well as collaborate as part of a productive team.I am an active and curious individual with a passion for learning and self-improvement.
           </p>
           </div>
         </div>
          <div className="team-image-card">
 
            <div className="leftTeamContainer">
-          <img src={kristtelle} alt="team-image" />
+          <img className="headshot" src={sean} alt="team-image" />
            <h3 className="name">Sean</h3> 
-          <h3>Software Developer</h3>
-          <ul>
+          <h3 className="role">Software Developer</h3>
+          <ul className="social">
+          <li>
+          <a href="mailto:sean.sipus@gmail.com" target="_blank"><img className="icon" src={mail} alt="" />
+              <span>E-mail</span></a> 
+            </li>
             <li>
-              <LinkedIn />
+            <a href="https://www.linkedin.com/in/sean-sipus/" target="_blank"><img className="icon" src={linkedin}/>
+             <span>Linkedin</span></a>
             </li>
           </ul>
           </div>
