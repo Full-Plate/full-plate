269fc0e66b33d34a27e124bac15c45a9071faa3b
emcdanie <66241957+emcdanie@users.noreply.github.com>
2022-04-07 18:31:13 +0200

    add modal to recipe tile page

--- src/components/RecipeTile.js
+++ src/components/RecipeTile.js
@@ -1,30 +1,42 @@
-
 import React from "react";
+import { useState } from "react";
+
+
+//component
+import Modal from "../components/modal/Modal";
 
 import "../styles/RecipeTile.css";
 
 
-  function RecipeTile ({ recipe }) {
+
+
+function RecipeTile({recipe, Modal}) {
+   const [showModal, setShowModal] = useState(false)
+
+  const openModal = () => {
+     setShowModal(prev => !prev)
+ }
   return (
-    recipe.recipe.image && (
-      <div className="tile-card-container1">
-        <div className="tile-card">
-          <img
-            className="tile-recipeTile-image"
-            src={recipe.recipe.image}
-            alt={recipe.recipe.label}
-            onClick={() => window.open(recipe.recipe.url)}
-          />
-          <div className="tile-card-body">
-            <h2 className="tile-card-title ">
-              <p>{recipe.recipe.label}</p>
-              <p>{recipe.recipe.totalTime} Mins</p>
-            </h2>
-          </div>
-        </div>
-      </div>
+    recipe.recipe.image && ( 
+      <div className = "tile-card-container1" >
+        <div className = "tile-card" >
+          <img className = "tile-recipeTile-image"
+              src = {recipe.recipe.image}
+              alt = {recipe.recipe.label}
+              onClick = {openModal}/> 
+              <Modal showModal={showModal} setShowModal={setShowModal} />
+        <div className = "tile-card-body" >
+          <h2 className = "tile-card-title " >
+            <p> {recipe.recipe.label} </p> 
+            <p> {recipe.recipe.totalTime} Mins </p> 
+          </h2> 
+        </div> 
+      </div> 
+    </div>
     )
   );
 }
+
+
 
 export default RecipeTile;
