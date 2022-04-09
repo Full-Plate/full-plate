15c7a45fdd155fffa27b7ec027a963b002f5b27e
Sean Sipus <sean.sipus@gmail.com>
2022-04-09 01:12:52 -0400

    Add media queries to donate food page to make mobile responsive and to match closer to design

--- src/pages/Donate.jsx
+++ src/pages/Donate.jsx
@@ -7,34 +7,26 @@ import orginfo from "../assets/orginfo.png";
 export default function Donate() {
   return (
     <div className="wrapper">
-    <div className="donateContainer">
-      <h1 className="donate-header">Why Donate?</h1>
-       <div className="donateinfo-text">
-         <p> On average, we throw away <strong>103</strong> pounds of food
-          each year. <strong>103</strong> pounds is about a <stong>MONTH</stong> of meals for another person </p>
-          <p className = "donate-text" > Be it Suggestions, feedback, business opportunities or
-             even how our recipes made you hungry, we would love to hear from you!</p>
-              < div className = "nyccomfridgeimage">
-              <img src={orginfo} alt="cityfridgeorgimage" />
-             </div>
-              </div>
-            <div className="wrapper">
-              <h1 className="nearestfridge-header">Find your nearest fridge today!</h1>
-                 <p className = "fridgecount-text" > Current NYC Fridge Count</p>
-                 <div className = "orglink-container">
-                   <div className ="counter-image">
-                    <img src={counter} alt="cityfridgephoto" />
-                      <button className="donatebutton" onClick={() => window.open("https://nycfridge.com/")}> Take me there now</button>
-                  
-                </div>
-                <div className = "orglink-container">
-               <div className ="fridge-image">
-                    <img className="fridge" src={fridge} alt="cityfridgeimage" />
-                    </div>
-              </div>
-              </div>
-            </div>
-          </div>
+      <div className="donateContainer">
+        <div className="donateText">
+          <h1> Why Donate? </h1>
+          <p className="topText"> On average, we throw away 103 pounds of food
+              each year. 103 pounds is about a month of meals for another person </p>
+          <p> A better question: Why <span>not</span> donate</p>
+        </div>
+        <img src={orginfo} alt="cityfridgeorgimage" />
       </div>
+      <div className = "orglink-container">  
+        <div className="fridgeContainer">
+          <h2>Find your nearest fridge today!</h2>
+          <p className = "fridgecount-text" > Current NYC Fridge Count</p>
+          <img src={counter} alt="cityfridgephoto" />
+          <button className="donatebutton" onClick={() => window.open("httpnycfridge.com/")}> Take me there now</button>
+        </div>
+        <div className ="fridgeImage">
+         <img className="fridge" src={fridge} alt="cityfridgeimage"/>
+        </div>
+      </div>
+    </div>
   )
 }
