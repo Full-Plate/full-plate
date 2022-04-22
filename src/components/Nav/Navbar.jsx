8edf1ec8c9cb58b83b3fd542d5ca58164adc414c
Sean Sipus <sean.sipus@gmail.com>
2022-04-22 05:06:11 -0400

    Add function to make navbar fixed on scroll

--- src/components/Nav/Navbar.jsx
+++ src/components/Nav/Navbar.jsx
@@ -1,17 +1,27 @@
 import React, { useState } from "react";
 import Logo from "../../assets/logo.png";
 import { Link, NavLink } from "react-router-dom";
-import MenuIcon from '@mui/icons-material/Menu';
 
 import "../../styles/Navbar.css";
 
 import HamburgerMenu from "./HamburgerMenu";
 
 function Navbar() {
+  const [ fix, setFix ] = useState(false)
+
+  function setFixed() {
+    if (window.scrollY >= 10) {
+      setFix(true)
+    }else{
+      setFix(false)
+    }
+  }
+
+  window.addEventListener("scroll", setFixed)
 
   return (
     <>
-  <nav> 
+  <nav className={fix ? "navbar fixed" : "navbar"}> 
     <NavLink to ="/">
     <a href="" className="logo">
     <img className="logo" src={Logo} ></img> 
